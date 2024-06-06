708fe2006fd549c92a187b5eae42da327a4dc0d1
SebasDevPy <sebas_figura21@hotmail.com>
2024-06-06 20:35:59 -0300

    modified:   __pycache__/inputs.cpython-312.pyc 	modified:   inputs.py 	modified:   pacientes.csv

--- inputs.py
+++ inputs.py
@@ -1,19 +1,22 @@
-def validar_dni(dni: int) -> bool:
-    if dni is None:
+def validar_dni(dni: str) -> bool:
+    if "." in dni:
         return False
-    if dni.isdigit() and len(dni) == 8:
-        dni = "0" * (8 - len(dni)) + dni
+    try:
+        dni = str(int(dni)) 
+        dni = "0" * (8 - len(dni)) + dni  
         return 4000000 <= int(dni) <= 99999999
-    return False
-        
+    except ValueError:
+        return False
 
 def validar_altura(altura: int) -> bool:
-    if altura.isdigit() == False:
+    try:
+        altura_int = int(altura)
+        return 30 <= altura_int <= 230
+    except ValueError:
         return False
-    altura_int = int(altura) 
-    return 30<= altura_int <= 230
 
 def validar_peso(peso: float) -> bool:
+    peso = peso.replace(",", ".")
     try:
         peso_float = float(peso)
         return 10.0 <= peso_float <= 300.0
@@ -21,16 +24,22 @@ def validar_peso(peso: float) -> bool:
         return False
     
 def validar_grupo_sanguineo(sangre: str) -> bool:
+    
+    sangre = sangre.replace(" ", "").upper()
     grupo_sanguineo = ["A+", "A-", "B+", "B-", "AB+", "AB-", "0+", "0-"]
     return sangre in grupo_sanguineo
 
 def validar_nombre_apellido(nombre_completo: str):
-    return nombre_completo.isalpha() and nombre_completo[0].isupper() and len(nombre_completo) <= 20
+    nombre_completo = nombre_completo.capitalize()
+    if nombre_completo.isalpha() or "'" in nombre_completo:
+        if nombre_completo == nombre_completo.capitalize() and len(nombre_completo) <= 20:
+            return True
+    return False
 
-def validar_edad(edad: int):
-    if edad.isdigit() == False:
+def validar_edad(edad: int) -> bool:
+    try:
+        edad_int = int(edad)
+        return 1 <= edad_int <= 120
+    except ValueError:
         return False
-    edad_int = int(edad)
-    return 1 <= edad_int <= 120
-
 
